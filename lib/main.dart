dc93eb5fb84e6c381ddfd31d252fd2468788f5fd
Matheus Melo <matheusfernando.vm@gmail.com>
2024-09-20 23:17:48 -0300

    Finalizando a entrega da prova

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
       initialRoute: "/",
       routes: {
         '/': (context) => FirstPage(),
-        'login': (context) => FirstPage(),
+        '/login': (context) => LoginPage(),
+        '/notasAluno': (context) => NotasAlunoPage(),
       },
     );
   }
@@ -29,13 +30,12 @@ class FirstPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            // um retângulo para separar widgets
             Image.asset('assets/images/logo.png'),
             const SizedBox(height: 10.0),
             ElevatedButton(
                 child: Text('Acessar'),
                 onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/');
+                  Navigator.pushReplacementNamed(context, '/login');
                 }),
           ],
         ),
@@ -52,21 +52,245 @@ class LoginPage extends StatefulWidget {
 }
 
 class LoginPageState extends State<LoginPage> {
-  final TextEditingController _emailDigitado = TextEditingController();
-  final TextEditingController _senhaDigitado = TextEditingController();
-  String _emailRecuperado = '';
-  String _senhaRecuperada = '';
-  String _tokenRecuperado = '';
+  String token = "";
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _senha = TextEditingController();
 
   Future<void> fetchData() async {
-    final response = await http.get(Uri.parse(""));
+    final response =
+        await http.get(Uri.parse("https://demo9989392.mockable.io/login"));
 
     if (response.statusCode == 200) {
       final jsonResponse = jsonDecode(response.body);
       setState(() {
-        _emailRecuperado = jsonResponse.email;
-        _senhaRecuperada = jsonResponse.senha;
+        token = jsonResponse['token'];
       });
+    } else {
+      print("Requisição falhou com o status: ${response.statusCode}.");
     }
+  }
+
+  void _enviar() async {
+    String email = _email.text;
+    String senha = _senha.text;
+
+    if (email.isNotEmpty && senha.isNotEmpty) {
+      await fetchData();
+      if (token.isNotEmpty) {
+        // ignore: use_build_context_synchronously
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => TokenPage(token: token)));
+      }
+    } else {
+      Navigator.pushReplacementNamed(context, '/login');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(height: 200),
+            SizedBox(
+                width: 300,
+                child: TextField(
+                  controller: _email,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                      hintText: 'Login',
+                      prefixIcon: Icon(Icons.account_circle_outlined),
+                      enabledBorder: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder()),
+                )),
+            const SizedBox(height: 16.0),
+            const SizedBox(height: 16.0),
+            SizedBox(
+                width: 300,
+                child: TextField(
+                  controller: _senha,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                      hintText: 'Senha',
+                      prefixIcon: Icon(Icons.account_circle_outlined),
+                      enabledBorder: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder()),
+                )),
+            const SizedBox(height: 100.0),
+            SizedBox(
+                width: 300,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: _enviar,
+                      child: const Text('Enviar'),
+                    ),
+                  ],
+                )),
+            const SizedBox(height: 16.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TokenPage extends StatelessWidget {
+  final String token;
+  const TokenPage({super.key, required this.token});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Align(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              'Token Recebido',
+              style: TextStyle(fontSize: 24.0, color: Colors.black),
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            Text(
+              token,
+              style: const TextStyle(fontSize: 24.0, color: Colors.black54),
+            ),
+            const SizedBox(height: 100),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/notasAluno');
+              },
+              child: const Text('Continuar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NotasAlunoPage extends StatefulWidget {
+  const NotasAlunoPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => NotasAlunoPageState();
+}
+
+class NotasAlunoPageState extends State<NotasAlunoPage> {
+  List<dynamic> listaAlunos = [];
+  List<dynamic> listaFiltrada = [];
+
+  Future<void> fetchData() async {
+    final response = await http
+        .get(Uri.parse("https://demo9989392.mockable.io/notasAlunos"));
+
+    if (response.statusCode == 200) {
+      final jsonResponse = jsonDecode(response.body);
+
+      setState(() {
+        listaAlunos = jsonResponse;
+        listaFiltrada = listaAlunos;
+      });
+    } else {
+      print("Requisição falhou com o status: ${response.statusCode}.");
+    }
+  }
+
+  void _filtarNotasMenoresQue60() {
+    setState(() {
+      listaFiltrada = listaAlunos.where((aluno) => aluno['nota'] < 60).toList();
+    });
+  }
+
+  void _filtarNotasEntre60E99() {
+    setState(() {
+      listaFiltrada = listaAlunos
+          .where((aluno) => aluno['nota'] >= 60 && aluno['nota'] < 100)
+          .toList();
+    });
+  }
+
+  void _filtarNotasIguais100() {
+    setState(() {
+      listaFiltrada =
+          listaAlunos.where((aluno) => aluno['nota'] == 100).toList();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: listaFiltrada.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final aluno = listaFiltrada[index];
+                  return Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 8.0, horizontal: 16.0),
+                    decoration: BoxDecoration(
+                      color: (aluno['nota'] < 60
+                          ? Colors.yellow
+                          : aluno['nota'] < 100
+                              ? Colors.blue
+                              : Colors.green),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: ListTile(
+                      title: Text("Matricula: ${aluno['matricula']}"),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("  Nome: ${aluno['nome']}"),
+                          Text("  Nota: ${aluno['nota']}")
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: _filtarNotasMenoresQue60,
+                    child: Text('< 60'),
+                  ),
+                  ElevatedButton(
+                    onPressed: _filtarNotasEntre60E99,
+                    child: Text('>=60'),
+                  ),
+                  ElevatedButton(
+                    onPressed: _filtarNotasIguais100,
+                    child: Text('=100'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
